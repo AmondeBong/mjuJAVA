bfd1ef0169f90826c1ae530bf37f98fc4bf4badf
AmondeBong <oms1218ju@gmail.com>
2022-09-20 11:15:56 +0900

    22.09.20.2 mju java

--- src/mjuJAVA/s60205210_Ominsu_lab2.java
+++ src/mjuJAVA/s60205210_Ominsu_lab2.java
@@ -13,30 +13,42 @@ public class s60205210_Ominsu_lab2 {
 		
 		Scanner sc = new Scanner(System.in);
 		int num;
+		boolean finish = true ;
 		double cel,fah;
-		System.out.println("==========================");
-		System.out.println("1. 화씨 -> 섭씨");
-		System.out.println("2. 섭씨 -> 화씨");
-		System.out.println("==========================");
-		System.out.print("번호를 선택하시오:");
-		num = sc.nextInt();
+		while(finish) {
+			System.out.println("==========================");
+			System.out.println("1. 화씨 -> 섭씨");
+			System.out.println("2. 섭씨 -> 화씨");
+			System.out.println("3. 끝내기");
+			System.out.println("==========================");
+			System.out.print("번호를 선택하시오:");
+			num = sc.nextInt();
 		
-		if(num == 1) {
-			System.out.print("화씨 온도를 입력하시오: ");
-			fah = sc.nextDouble();
-			cel = (fah-32)*5/9;
-			System.out.println("섭씨온도는 "+ cel);
-		} else {
-			System.out.print("섭씨 온도를 입력하시오: ");
-			cel = sc.nextDouble();
-			fah = (cel*9/5)+32;
-			System.out.println("화씨온도는 "+ fah);
-		}
+			if(num == 1) {
+				System.out.print("화씨 온도를 입력하시오: ");
+				fah = sc.nextDouble();
+				cel = (fah-32)*5/9;
+				System.out.println("섭씨온도는 "+ cel);
+			} 	
+			else if(num==2){
+				System.out.print("섭씨 온도를 입력하시오: ");
+				cel = sc.nextDouble();
+				fah = (cel*9/5)+32;
+				System.out.println("화씨온도는 "+ fah);
+			} 
+			else if(num==3) {
+				System.out.println("온도 변환 프로그램을 종료합니다.");
+				finish = false;
+			} 
+			
+			else {
+			System.out.println("1 2 3 중에서 선택해 주세요.");
+			}
 		
 	}
 	
 	
-	
+	}
 	
 	
 }
