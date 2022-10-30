13968e4046d1a92554dca5cee51ca6368b25515d
AmondeBong <oms1218ju@gmail.com>
2022-10-30 20:27:41 +0900

    22.10.30 MJU JAVA

---
+++ src/mjuJAVA/MountainBike.java
@@ -0,0 +1,79 @@
+package mjuJAVA;
+/** A short description of the program
+ * @SID : 60205210
+ * @author O minsu
+ * @assignment JAVA Programming - 6
+ * @data 20221030
+ */
+
+class Bicycle{
+	private int Gear;
+	private int speed;
+	
+	public Bicycle(){
+		Gear = 0;
+		speed = 0;
+	}
+	
+	public Bicycle(int Gear, int speed){
+		this.Gear = Gear;
+		this.speed = speed;
+	}
+	
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	
+	public void setGear(int gear) {
+		this.Gear = gear;
+	}
+	
+	public int getSpeed() {
+		return speed;
+	}
+	
+	public int getGear() {
+		return Gear;
+	}
+	
+	public int plusSpeed(int value) {
+		return this.speed += value;
+	}
+	
+	public int subSpeed(int value) {
+		return this.speed -= value;
+	}
+	
+	public int plusGear(int value) {
+		return this.Gear += value;
+	}
+	
+	public int subGear(int value) {
+		return this.Gear -= value;
+	}
+	
+	
+}
+public class MountainBike extends Bicycle {
+	private int seatHeight;
+	
+	public MountainBike(int startHeight,int startSpeed,int startGear) {
+		super(startGear,startSpeed);
+		this.seatHeight = startHeight;
+	}
+	
+	public void setSeatHeight(int seatHeight) {
+		this.seatHeight = seatHeight;
+	}
+	
+	public int getSeatHeight() {
+		return seatHeight;
+	}
+	
+	public String toString() {
+		return "속도 : "+getSpeed()+", 기어: "+getGear()+", 의자 높이: "+getSeatHeight();
+	}
+	
+}
+
+
---
+++ src/mjuJAVA/lab6.java
@@ -0,0 +1,22 @@
+package mjuJAVA;
+/** A short description of the program
+ * @SID : 60205210
+ * @author O minsu
+ * @assignment JAVA Programming - 6
+ * @data 20221030
+ */
+public class lab6 {
+	public static void main(String[] args) {
+		MountainBike mb = new MountainBike(10,10,2);
+		
+		for(int i=0;i<3;i++) {
+			mb.plusSpeed(5);
+		}
+		
+		for(int j=0;j<5;j++) {
+			mb.plusGear(1);
+		}
+		
+		System.out.println(mb.toString());
+	}
+}
