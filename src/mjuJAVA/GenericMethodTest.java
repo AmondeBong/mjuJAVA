d918b47f2269c4ca816fc830a68d655963135eef
AmondeBong <oms1218ju@gmail.com>
2022-11-29 10:03:36 +0900

    22.11.25 mju JAVA

--- src/mjuJAVA/GenericMethodTest.java
+++ src/mjuJAVA/GenericMethodTest.java
@@ -8,7 +8,7 @@ public class GenericMethodTest {
 		
 		T s1;
 		List<T> list1 = Arrays.asList(array);
-		Iterator e1 = list1.iterator();
+		Iterator<T> e1 = list1.iterator();
 		
 		while(e1.hasNext()) {
 			s1 = (T)e1.next();
@@ -19,7 +19,7 @@ public class GenericMethodTest {
 	public static void main(String[] args) {
 		Integer[] i = {10,20,30,40,50};
 		String a[] = new String[] {"A","B","C","D","E"};
-		List<String> list = Arrays.asList(a);
+		
 		
 		printArray(i);
 		printArray(a);
